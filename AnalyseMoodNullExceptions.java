3fa03b949dd8b27d78c3fda0e280768ea6028d21
Mohit Raj <rajmohit.129@gmail.com>
2022-07-26 07:13:45 +0530

    [Mohit]Added:Custom execption using enum

---
+++ AnalyseMoodNullExceptions.java
@@ -0,0 +1,13 @@
+public class AnalyseMoodNullExceptions extends Exception{
+
+    enum ExceptionType{
+        ENTERD_NULL, ENTERD_EMPTY
+    }
+
+    ExceptionType type;
+    public AnalyseMoodNullExceptions(ExceptionType type, String message)
+    {
+        super(message);
+        this.type = type;
+    }
+}
